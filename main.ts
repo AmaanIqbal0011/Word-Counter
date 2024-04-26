8ec086cb0d77f72d1496c07db17c58657742af7f
AmaanIqbal0011 <166907241+AmaanIqbal0011@users.noreply.github.com>
2024-04-26 23:39:55 +0500

    add shebang in js file

--- main.ts
+++ main.ts
@@ -2,15 +2,15 @@
 
 import inquirer from "inquirer";
 
-const answers : {
-    sentence : string
-} = await inquirer.prompt(
-    {
-        name : "sentence",
-        type : "input",
-        message : "Enter Your Sentence To Count The Word: "
-    }
-);
+const answers: {
+  sentence: string;
+} = await inquirer.prompt([
+  {
+    name: "sentence",
+    type: "input",
+    message: "Enter Your Sentence To Count The Word: ",
+  },
+]);
 
 const words = answers.sentence.trim().split(" ");
 console.log(words);
