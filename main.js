c4675517569ed86e5ba074dbe232b13b9d65b662
AmaanIqbal0011 <166907241+AmaanIqbal0011@users.noreply.github.com>
2024-04-26 23:19:28 +0500

    Developing Simple Word Counter App (CLI Base)

---
+++ main.js
@@ -0,0 +1,9 @@
+import inquirer from "inquirer";
+const answers = await inquirer.prompt({
+    name: "sentence",
+    type: "input",
+    message: "Enter Your Sentence To Count The Word: "
+});
+const words = answers.sentence.trim().split(" ");
+console.log(words);
+console.log(`Your Sentence Word Count is ${words.length}`);
